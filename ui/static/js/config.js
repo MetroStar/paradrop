e3230c02cb452400c7a2724b3cfb4befdacbcabe
marshyski <3740784+marshyski@users.noreply.github.com>
2024-08-06 21:07:34 -0400

    Fix MODEL_URL in config.js

--- ui/static/js/config.js
+++ ui/static/js/config.js
@@ -1,2 +1,2 @@
-export const URL = 'http://127.0.0.1:5000'
-export const MODEL_URL = 'https://54.159.74.29:8000/v1/models/rag?q='
+export const URL = "http://127.0.0.1:5000";
+export const MODEL_URL = "https://127.0.0.1:8444/v1/models/rag?q=";
